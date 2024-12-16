2333560508f839ca47ab42c47b162aa7fa467e60
masonkobilka <masonkobilka@gmail.com>
2024-12-16 07:29:07 -0600

    Add fps setting of 60

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    game_introduction()
 
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -14,7 +14,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        time_passed = clock.tick(60) / 1000
 
+def game_introduction():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
